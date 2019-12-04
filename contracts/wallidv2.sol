ce8ac4eab3fd7aadf5e6d9a69d1ea10af1055774
Vitor Viana <vitor.viana@wallid.io>
2019-12-04 12:13:55 +0000

    update contract versino

--- contracts/wallidv2.sol
+++ contracts/wallidv2.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity >=0.4.25 <0.6.0;
 
 /**
  *  Created Nov-19 
@@ -9,26 +9,23 @@ pragma solidity >=0.4.22 <0.6.0;
  */
 contract WalletIdHydrid 
 {
-    uint DriftPeriod = 9000;
-    uint dataInsertions = 0;
-    mapping(address => IdtData) _identities;
-    
+    uint256 constant DriftPeriod = 9000;
+    uint256  dataInsertions = 0;
+    mapping(address => IdtData)  _identities;
+  
     struct  IdtData 
     {
-        mapping(bytes1 => UserData)  idt_type;    
+        mapping(bytes32 => UserData)  idt_type;    
     }
-    
     
     struct UserData {
-        bytes32 [10] datablocks;
-        bytes1 idt;
-        bytes1 sid;
-        uint opid;
         uint256 updateAt;
+        bytes32 idt;
+        bytes32 sid;
+        bytes [10] dBlocks;
     }
     
-    
-   /**
+    /**
     * params : 
     *    - idt -> type of identity (cc_pt, cc_tst)
     *    - idx -> data block idx
@@ -37,24 +34,22 @@ contract WalletIdHydrid
     *    - sid  -> storeid
     */
   
-   function addUserBlock(bytes1 idt, uint8 idx , bytes32 dBlock ,  uint opid, bytes1 sid ) public returns (uint8 code)
+   function addUserBlock(address  uIdx, bytes32 idt, uint8 idx , bytes calldata dblock , bytes32 sid ) external returns (uint256 sucess, bytes32 code)
    {
        UserData memory  uData;
        uData.idt = idt;
        uData.sid = sid;
-       uData.opid = opid;
-       uData.datablocks[idx] = dBlock;
-       uData.updateAt = now;
+       uData.dBlocks[idx] = dblock;
        
-       //Initialize idt's list for WA
-       IdtData storage iData = _identities[msg.sender];
+       //Initialize idt's list for WA 
+       IdtData storage iData = _identities[uIdx];
        //add user information
        iData.idt_type[idt] = uData;
        
        //incr total datablocks
-       dataInsertions = dataInsertions +1;
+       dataInsertions = dataInsertions + 1;
        
-       return 1;
+       return (1 , 'Data was sucessful updated' );
    }
    
    /**
@@ -70,24 +65,24 @@ contract WalletIdHydrid
     *   https://medium.com/coinmonks/solidity-tutorial-returning-structs-from-public-functions-e78e48efb378
     * 
     **/
-   function getUserBlock(bytes1 idt) public view returns (address, bytes32[10] memory, bytes1,  bytes1, uint)
+   function getUserBlock(address uIdx, bytes32 idt, uint8 idx) external view returns (uint256 sucess, bytes memory uBlock, address o_uIdx, bytes32 o_sid,  bytes32 o_idt)
    {
-       //Testing out parameters
-       /*
-       bytes32[] memory arr = new bytes32[](10);
-       address addr = msg.sender;
-       bytes1  i = '1';
-       bytes1  s = '1';
-       uint8   o = 0;
-       */
-       
+    
        //Getting all idt's for an wallet address
-       IdtData  storage _idtDataTmp =  _identities[msg.sender];
+       IdtData  storage _idtDataTmp =  _identities[uIdx];
        //Getting user idt_type data
        UserData storage uBlockData = _idtDataTmp.idt_type[idt];
+       
+       //checking idx are in range
+       if(idx > 9 && idx < 0)
+       {
+           return (0, bytes(''), uIdx, uBlockData.sid, uBlockData.idt);
+       }
     
-       return (msg.sender, uBlockData.datablocks, uBlockData.idt,  uBlockData.sid, uBlockData.opid );
+        return (1, uBlockData.dBlocks[idx],   uIdx, uBlockData.sid, uBlockData.idt);
    }
+   
+   
    
    function decodeIdx (uint256 idxN, uint nounce) public view returns (uint) {
         uint ts = now * 1000;
@@ -98,7 +93,14 @@ contract WalletIdHydrid
         
         return idxN - idxOffset;
     }
+    
+  
    
+    function totalInsertions()  public view returns (uint s){
+       
+     return dataInsertions;
+    }
+    
 
     
 }
