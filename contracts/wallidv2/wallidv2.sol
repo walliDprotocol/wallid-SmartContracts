153dcb4d842f8d8adf05fd68e30d793527b9eba6
Vitor Viana <vitor.viana@wallid.io>
2020-01-03 16:34:16 +0000

    update contract for new version

--- contracts/wallidv2/wallidv2.sol
+++ contracts/wallidv2/wallidv2.sol
@@ -13,19 +13,18 @@ contract WalletIdHydrid
     uint256  dataInsertions = 0;
     //user data mapping idxN -> idt -> data
     mapping(uint256 => IdtData)  _identities;
-    string _texto;
-  
+
+
     struct  IdtData 
     {
         mapping(bytes32 => UserData)  idt_type;    
     }
     
     struct UserData {
-        uint256 updateAt;
-        bytes32 idt;
         bytes32 sid;
         bytes [10] dBlocks;
     }
+    
     
     /**
     * params : 
@@ -40,15 +39,10 @@ contract WalletIdHydrid
   
    function addUserBlock(uint256  uIdx, bytes32 idt, uint8 idx , bytes calldata dblock , bytes32 sid ) external returns (uint256 sucess, bytes32 code)
    {
-       UserData memory  uData;
-       uData.idt = idt;
-       uData.sid = sid;
-       uData.dBlocks[idx] = dblock;
-       
        //Initialize idt's list for WA 
        IdtData storage iData = _identities[uIdx];
-       //add user information
-       iData.idt_type[idt] = uData;
+       iData.idt_type[idt].sid = sid;
+       iData.idt_type[idt].dBlocks[idx] = dblock;
        
        //incr total datablocks
        dataInsertions = dataInsertions + 1;
@@ -82,10 +76,10 @@ contract WalletIdHydrid
        //checking idx are in range
        if(idx > 9 && idx < 0)
        {
-           return (0, bytes(''), uIdx, uBlockData.sid, uBlockData.idt);
+           return (0, bytes(''), uIdx, uBlockData.sid, idt);
        }
     
-        return (1, uBlockData.dBlocks[idx],   uIdx, uBlockData.sid, uBlockData.idt);
+        return (1, uBlockData.dBlocks[idx],   uIdx, uBlockData.sid, idt);
    }
    
    
